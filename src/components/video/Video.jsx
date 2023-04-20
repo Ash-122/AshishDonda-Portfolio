97b0b09bdb1adbd608724d55021a56920241f481
Ash-122 <ashishdonda122@gmail.com>
2023-04-20 11:50:35 -0400

    video controls update

--- src/components/video/Video.jsx
+++ src/components/video/Video.jsx
@@ -1,15 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './video.css';
 import intro from '..//..//assets/intro.mp4';
 
 const Video = () => {
+  const [isMuted, setIsMuted] = useState(true);
+  const videoRef = useRef(null);
+
   useEffect(() => {
-    const video = document.getElementById('myVideo');
+    const video = videoRef.current;
     video.addEventListener('ended', handleVideoEnded);
     video.play();
 
     const handleMouseDown = () => {
-      video.remove();
+      video.pause();
       document.removeEventListener('mousedown', handleMouseDown);
     };
 
@@ -24,19 +27,44 @@ const Video = () => {
   const handleVideoEnded = () => {
     const rootElement = document.getElementById('root');
     rootElement.classList.remove('hidden');
-    const video = document.getElementById('myVideo');
+    const video = videoRef.current;
     video.remove();
   };
 
+  const handleMuteToggle = () => {
+    const video = videoRef.current;
+    video.muted = !video.muted;
+    setIsMuted(video.muted);
+  };
+
+  const handleMouseOver = () => {
+    const video = videoRef.current;
+    video.pause();
+  };
+
+  const handleMouseOut = () => {
+    const video = videoRef.current;
+    video.play();
+  };
+
   return (
-    
-    <div class="vid_1_wrapper">
-        <video id="myVideo" autoPlay muted>
-        <source src={intro} type="video/mp4"  />
-        <div class="vid_1_text">Click to STOP</div>
+    <div className="vid_1_wrapper">
+      <video
+        id="myVideo"
+        ref={videoRef}
+        autoPlay
+        muted={isMuted}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      >
+        <source src={intro} type="video/mp4" />
       </video>
+      <div className="unmute-btn" onClick={handleMuteToggle}>
+        {isMuted ? 'Unmute' : 'Mute'}
+      </div>
+      <div className="overlay-text">Click anywhere to STOP the video</div>
     </div>
   );
 };
 
-export default Video
+export default Video;
